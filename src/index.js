536cf05cf8a9a8a182f9b0ecb1c8ed1a76b69319
Laza Bogdan <laza.bogdan@macro-net.ro>
2016-01-25 15:15:49 +0200

    cleanup

--- src/index.js
+++ src/index.js
@@ -5,14 +5,8 @@ let VueMarkdownEditor = {
 	MarkdownEditor
 }
 
-// Load directives
-// import MyDirective from './directives/my-directive'
-
 // Setup
-VueMarkdownEditor.install = function (Vue) {
-	// Register directives
-	// Vue.directive('my-directive', MyDirective)
-}
+VueMarkdownEditor.install = function (Vue) {}
 
 // auto install in browser
 if (typeof window !== 'undefined' && window.Vue) {
